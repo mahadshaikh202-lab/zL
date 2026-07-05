7c8e45b15067b979514d0e24c73c01233a2975e9
mahadshaikh202-lab <mahadshaikh202@gmail.com>
2026-07-05 05:13:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// ===============================
+// Navbar Scroll Effect
+// ===============================
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+
+    }else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
+// ===============================
+// Cursor Glow
+// ===============================
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+
+});
